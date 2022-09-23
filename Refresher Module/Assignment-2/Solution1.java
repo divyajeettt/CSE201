19bee9fab48d40a4ec36e07e3b35d666d5c1b513
Divyajeet Singh <knightt1821@gmail.com>
2022-09-23 11:54:55 +0530

    Update and rename Question1.java to Solution1.java

--- Refresher Module/Assignment-2/Solution1.java
+++ Refresher Module/Assignment-2/Solution1.java
@@ -37,7 +37,7 @@ class Student {
 }
 
 
-public class Question1 {
+public class Solution1 {
     public static void main(String[] args) {
         Student s1 = new Student();
         s1.name = "Crimge";
